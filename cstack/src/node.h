ed0e2a57814b70b21840795c774f116ae6770ed3
Hari Krishnan <codeharik@gmail.com>
2025-01-26 21:56:36 +0530

    btree db

--- cstack/src/node.h
+++ cstack/src/node.h
@@ -58,6 +58,10 @@ const uint32_t INTERNAL_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + INTERNAL_NO
 const uint32_t INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
 const uint32_t INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
 const uint32_t INTERNAL_NODE_CELL_SIZE = INTERNAL_NODE_CHILD_SIZE + INTERNAL_NODE_KEY_SIZE;
+/* Keep this small for testing */
+const uint32_t INTERNAL_NODE_MAX_CELLS = 3;
+
+uint32_t *node_parent(void *node) { return node + PARENT_POINTER_OFFSET; }
 
 uint32_t *leaf_node_num_cells(void *node)
 {
@@ -152,7 +156,7 @@ uint32_t *internal_node_child(void *node, uint32_t child_num)
 
 uint32_t *internal_node_key(void *node, uint32_t key_num)
 {
-    return internal_node_cell(node, key_num) + INTERNAL_NODE_CHILD_SIZE;
+    return (void *)internal_node_cell(node, key_num) + INTERNAL_NODE_CHILD_SIZE;
 }
 
 uint32_t get_node_max_key(void *node)
